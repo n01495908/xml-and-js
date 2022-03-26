7d81d6dc05216a2ad993199ba5e42d4e8d356132
n01495908 <2937priya@gmail.com>
2022-03-25 22:42:13 -0400

    commit

---
+++ week-10/assignments/Assignment/Server.js
@@ -0,0 +1,41 @@
+const http = require("http");
+
+const people = require("./people.json");
+
+const parseURLParams = (value) => {
+  const params = new URLSearchParams(value);
+
+  return Array.from(params.entries()).reduce(
+    (acc, [key, value]) => ({ ...acc, [key]: value }),
+    {}
+  );
+};
+
+const server = http.createServer(async (req, res) => {
+  const [basePath, paramsString] = req.url.split("?");
+
+  if (basePath === "/api/people" && req.method === "GET") {
+    const params = parseURLParams(paramsString);
+
+    const { data, code } = await people.getAll(params);
+
+    res.writeHead(code, { "Content-Type": "application/json" });
+    res.end(data);
+  } else if (basePath.match(/\/api\/people\/\w+/) && req.method === "GET") {
+    const id = basePath.split("/")[3];
+
+    const { data, code } = await people.getById(id);
+
+    res.writeHead(code, { "Content-Type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route Not Found" }));
+  }
+});
+
+const PORT = 8080;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = server;
